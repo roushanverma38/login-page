d0d0c89647d3b80d6517293a4625eeb35c07b17d
user <rishikasinghrajputana@gmail.com>
2022-03-04 18:46:40 +0530

    login page

---
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /*setContentView(R.layout.activity_main);*/
+        setContentView(R.layout.activity_register);
+
+    }
+
+    public void logout(View view ){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),login.class));
+        finish();
+    }
+}
